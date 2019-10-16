e616712ba28b552c5f4072cc95c6322d746a492a
lucguittard <lucguittard@users.noreply.github.com>
2019-10-16 16:06:14 -0600

    day 3 commit

--- APP/app.py
+++ APP/app.py
@@ -1,19 +1,40 @@
 """Main application for twitoff"""
-
-#imports 
-from flask import Flask
-from.models import DB
-
-# create app factory
+#imports
+from decouple import config
+from flask import Flask, render_template, request
+from .models import DB, User
+from .twitter import add_or_update_user
 def create_app():
-    """Creates and configs an instance of a flask app"""
+    """create and configures an instance of a flask app"""
     app = Flask(__name__)
-
-    # configure the app
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    #app.config['ENV'] = config('ENV') #should change this later to production
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
     DB.init_app(app)
-
-    @app.route("/")
+    @app.route('/')
     def root():
-        return "Welcome"
+        users = User.query.all()
+        return render_template('base.html', title='Home', users=users)
+    @app.route('/user', methods=['POST','GET'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None,message=''):
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = "User {} successfully added!".format(name)
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = "Error adding {}: {}".format(name,e)
+            tweets=[]
+        return render_template('user.html', title=name, tweets=tweets,
+        message=message)
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='DB Reset', users=[])
     return app
+#Collapse
+
+
+
--- APP/models.py
+++ APP/models.py
@@ -1,16 +1,19 @@
-"""SQLAlchemy models for Twitoff"""
-
+"""SQLALchemy models for Twitoff"""
 from flask_sqlalchemy import SQLAlchemy
-
 DB = SQLAlchemy()
-
 class User(DB.Model):
-    """Twitter users that are pulled in and analyzed"""
-    id = DB.Column(DB.Integer, primary_key = True)
-    name = DB.Column(DB.String(15), nullable = False)
-
+    """Twitter users that we pull and analyze"""
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    name = DB.Column(DB.String(15), nullable=False)
+    newest_tweet_id = DB.Column(DB.BigInteger)
+    def __repr__(self):
+        return '<User {}>'.format(self.name)
 class Tweet(DB.Model):
     """Tweets"""
-    id = DB.Column(DB.Integer, primary_key = True)
-    text = DB.Column(DB.Unicode(280))
-
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    text = DB.Column(DB.Unicode(300))
+    embedding = DB.Column(DB.PickleType, nullable = False)
+    user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
+    user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
+    def __repr__(self):
+	       return '<Tweet {}>'.format(self.text)
