2db8432c15bef169fffe369b3ef70aa27865b759
lucguittard <lucguittard@users.noreply.github.com>
2019-10-17 11:29:53 -0600

    a new app

--- APP/app.py
+++ APP/app.py
@@ -1,9 +1,10 @@
-#Here's also an updated app.py for your reference with just the POST method in the first line
 """Main application for twitoff"""
 #imports
+#import pickle
 from decouple import config
 from flask import Flask, render_template, request
 from .models import DB, User
+from .predict import predict_user
 from .twitter import add_or_update_user
 def create_app():
     """create and configures an instance of a flask app"""
@@ -31,9 +32,24 @@ def create_app():
             tweets=[]
         return render_template('user.html', title=name, tweets=tweets,
         message=message)
+    @app.route('/compare', methods=['POST'])
+    def compare(message=''):
+        user1, user2 = sorted([request.values['user1'],
+                              request.values['user2']])
+        if user1 == user2:
+            message = 'Cannot compare a user to themselves!'
+        else:
+            prediction = predict_user(user1, user2, request.values['tweet_text'])
+            # prediction = predict_user(user1, user2, request.values['tweet_text'])
+            # CACHED_COMPARISONS
+
+            message = '"{}" is more likely to be said by {} than {}'.format(
+                request.values['tweet_text'], user1 if prediction else user2,
+                user2 if prediction else user1)
+        return render_template('prediction.html', title='Prediction', message=message)
     @app.route('/reset')
     def reset():
         DB.drop_all()
         DB.create_all()
         return render_template('base.html', title='DB Reset', users=[])
-    return app 
+    return app
--- APP/predict.py
+++ APP/predict.py
@@ -19,5 +19,5 @@ def predict_user(user1_name, user2_name, tweet_text, cache=None):
                                  np.zeros(len(user2.tweets))])
         log_reg = LogisticRegression().fit(embeddings, labels)
         cache and cache.set(user_set, pickle.dumps(log_reg))
-    tweet_embeddings = BASILICA.embed_sentence(tweet_text, model='twitter')
+    tweet_embedding = BASILICA.embed_sentence(tweet_text, model='twitter')
     return log_reg.predict(np.array(tweet_embedding).reshape(1, -1))
